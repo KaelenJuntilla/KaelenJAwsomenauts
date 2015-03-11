c610a6ee1c5c4c5fac4b60b815d46c1d850df714
Kaelen Juntilla <Da4kast@charter.net>
2015-03-11 15:01:29 -0700

    6th push

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -17,6 +17,7 @@ game.PlayerEntity = me.Entity.extend({
 
         this.renderable.addAnimation("idle", [78]);
         this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
+        this.renderable.addAnimation("attack", [65, 66, 67, 68, 69, 70, 71, 72], 80);
 
         this.renderable.setCurrentAnimation("idle");
     },
@@ -30,17 +31,37 @@ game.PlayerEntity = me.Entity.extend({
         } else {
             this.body.vel.x = 0;
         }
-
-        if (this.body.vel.x !== 0) {
-            if (!this.renderable.isCurrentAnimation("walk")) {
+         
+        if(me.input.isKeyPressed("attack")){
+            if(!this.renderable.isCurrentAnimation("attack")){
+                //Sets the current Animation to attack and once that is over
+                //goes back to the idle animation//
+                this.renderable.setCurrentAnimation("attack", "idle");
+                //Makes it so that the next time we start this sequence we begin
+                //from the first animation, not wherever we left off when we switched to another animation//
+                this.renderable.setCurrentAnimation("walk");
+                this.renderable.setAnimationFrame();
+            }
+        }
+        else if(this.body.vel.x !== 0){
+            if(!this.renderable.isCurrentAnimation("walk")) {
                 this.renderable.setCurrentAnimation("walk");
             }
-        } else {
+        }else{
             this.renderable.setCurrentAnimation("idle");
         }
 
-
-
+        if(me.input.isKeyPressed("attack")){
+            if(!this.renderable.isCurrentAnimation("attack")){
+                //Sets the current Animation to attack and once that is over
+                //goes back to the idle animation//
+                this.renderable.setCurrentAnimation("attack", "idle");
+                //Makes it so that the next time we start this sequence we begin
+                //from the first animation, not wherever we left off when we switched to another animation//
+                this.renderable.setCurrentAnimation("walk");
+                this.renderable.setAnimationFrame();
+            }
+        }
 
 
         this.body.update(delta);
