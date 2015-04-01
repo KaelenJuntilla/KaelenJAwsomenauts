f2ad5d7f515ed28f19c0ebeaba54e524895adb2b
Kaelen Juntilla <Da4kast@charter.net>
2015-04-01 10:47:57 -0700

    13th push

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -1,7 +1,29 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+game.GameManager = Object.extend({
+    init: function(x, y, settings){
+        this.now = new Date().getTime();
+        this.lastCreep = new Date().getTime();
+        console.log("GameManager");
+        this.alwaysUpdate = true;
+    },
+    
+    update:function(){
+        this.now = new Date().getTime();
+        
+        if(game.data.player.dead){
+            me.game.world.removeChild(game.data.player);
+            me.state.current().resetPlayer(10, 0);
+        }
+        
+        
+        if(Math.round(this.now/1000)%10 ===0 &&(this.now - this.lastCreep>= 1000)){
+            this.lastCreep = this.now;
+            var creepe = me.pool.pull("EnemyCreep", 1000, 0, {});
+            me.game.world.addChild(creepe, 5);
+            
+        }
+        
+        return true;
+    }
+});
 
 
